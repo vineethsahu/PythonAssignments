109dfb30e3a3536cf7539af5d79f2ef9d95b8ff8
vineethsahu <70767572+vineethsahu@users.noreply.github.com>
2021-05-17 02:01:09 +0530

    Add files via upload

---
+++ assignment4_list.py
@@ -0,0 +1,12 @@
+print('Enter elements in a list')
+
+l = [int(elements) for elements in input().split(",")]
+print("The list is: ", l)
+
+new_l = []
+for elements in l:
+    if elements>0:
+        new_l.append(elements)
+        print(elements)
+
+print("The new list is: ", new_l)
